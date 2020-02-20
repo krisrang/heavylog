ad4b7825080bd73592beac39fab3deb924e745a9
Kristjan Rang <mail@rang.ee>
2020-02-20 23:27:26 +0200

    More testss

--- spec/app/application.rb
+++ spec/app/application.rb
@@ -7,7 +7,7 @@ module Heavylog
     config.cache_classes = true
     config.consider_all_requests_local = true
     config.action_controller.perform_caching = false
-    config.action_dispatch.show_exceptions = false
+    config.action_dispatch.show_exceptions = true
     config.action_controller.allow_forgery_protection = false
     config.active_support.deprecation = :stderr
     config.hosts.clear
@@ -24,6 +24,10 @@ class TestController < ActionController::Base
   def redirect_action
     redirect_to "http://redirected.com"
   end
+
+  def raise_action
+    raise StandardError, "This action raises an exception"
+  end
 end
 
 Rails.application.initialize!
@@ -31,4 +35,5 @@ Rails.application.initialize!
 Rails.application.routes.draw do
   get "/test" => "test#test_action"
   get "/redirect" => "test#redirect_action"
+  get "/raise" => "test#raise_action"
 end
