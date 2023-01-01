f9194b7ec79dddcec678f6d576ca0fcdf241bae5
Kristjan Rang <mail@rang.ee>
2023-01-01 19:18:49 +0200

    Use request.remote_ip.

--- lib/heavylog/middleware.rb
+++ lib/heavylog/middleware.rb
@@ -18,7 +18,7 @@ module Heavylog
         request = ActionDispatch::Request.new(env)
         RequestStore.store[:heavylog_request_id] = request.uuid
         RequestStore.store[:heavylog_request_start] = Time.now.iso8601
-        RequestStore.store[:heavylog_request_ip] = request.ip
+        RequestStore.store[:heavylog_request_ip] = request.remote_ip
       end
 
       @app.call(env)
